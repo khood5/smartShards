1728d129730ccc0b499d2b2873f0c8ec845e0503
Kendric Hood <khood5@kent.edu>
2020-03-23 02:39:29 -0400

    Tests are written for committee setup. Joins and leaves are next.

--- src/SawtoothPBFT.py
+++ src/SawtoothPBFT.py
@@ -1,15 +1,16 @@
 import docker
+import json
 
 # these commands are used to create the genesis block for a PBFT committee, they are listed in the order they should be run
 # they only need to be executed once on one peer
 # some commands (consensus and pbft config) need to have the list of peers (by there keys) appended to the end between
 # '' including the peer that is creating the genesis block
-SAWTOOTH_GENESIS_COMMANDS = {"genesis": "sawset genesis --key $HOME/.sawtooth/keys/root.priv -o config-genesis.batch",
-                             "consensus_config": "sawset proposal create --key $HOME/.sawtooth/keys/root.priv -o config-consensus.batch \
+SAWTOOTH_GENESIS_COMMANDS = {"genesis": "sawset genesis --key /root/.sawtooth/keys/root.priv -o config-genesis.batch",
+                             "consensus_config": "sawset proposal create --key /root/.sawtooth/keys/root.priv -o config-consensus.batch \
                                                                               sawtooth.consensus.algorithm.name=pbft \
                                                                               sawtooth.consensus.algorithm.version=1.0 \
                                                                               sawtooth.consensus.pbft.members=",
-                             "pbft_config": "sawset proposal create --key $HOME/.sawtooth/keys/root.priv -o pbft-settings.batch \
+                             "pbft_config": "sawset proposal create --key /root/.sawtooth/keys/root.priv -o pbft-settings.batch \
                                                                               sawtooth.consensus.algorithm.name=pbft \
                                                                               sawtooth.consensus.algorithm.version=1.0 \
                                                                               sawtooth.consensus.pbft.members=",
@@ -22,19 +23,14 @@ SAWTOOTH_GENESIS_COMMANDS = {"genesis": "sawset genesis --key $HOME/.sawtooth/ke
 # all commands should end with a &
 SAWTOOTH_START_COMMANDS = {"validator": 'sawtooth-validator \
                             --bind component:tcp://127.0.0.1:4004 \
-                            --bind network:tcp://$(hostname -i)8800 \
-                            --bind consensus:tcp://$(hostname -i):5050 \
-                            --endpoint tcp://$(hostname -i):8800 \
+                            --bind network:tcp://__IP__:8800 \
+                            --bind consensus:tcp://__IP__:5050 \
+                            --endpoint tcp://__IP__:8800 \
                             --peers ',
-                           "api": 'sawtooth-rest-api -v &',
-                           "transaction_processor": 'settings-tp -v &',
-                           "client": 'intkey-tp-python -v &',
-                           "pbft": 'pbft-engine -vv --connect tcp://$(hostname -i):5050 &'}
-
-
-# runs a command in a docker container
-def run_command(container: docker.DockerClient.containers, command: str):
-    return container.exec_run(command).output.decode('utf-8').strip()
+                           "api": 'sawtooth-rest-api -v',
+                           "transaction_processor": 'settings-tp -v',
+                           "client": 'intkey-tp-python -v',
+                           "pbft": 'pbft-engine -vv --connect tcp://__IP__:5050'}
 
 
 # takes a list of containers and a command appends the validator.pub keys to the end of the command
@@ -43,20 +39,8 @@ def append_keys(containers, command):
     keys = []
     for c in containers:
         keys.append(c.key())
-    keys = str(keys)  # converts list to string in form ['keyVal','keyVal' ...]
+    keys = str(keys).replace('\'', '\"')  # converts list to string in form ["keyVal","keyVal" ...]
     return command + '\'{}\''.format(keys)
-
-
-# takes a list of containers and a command appends the endpoints of each sawtooth instance to the command
-# endpoints are added in the form [tcp://172.17.0.2:8800,tcp://172.17.0.3:8800, ...]
-def append_endpoints(containers, command):
-    ips = []
-    for c in containers:
-        ips.append(c.ip)
-    for i in range(len(ips)):
-        ips[i] = "tcp://{}:8800".format(ips[i])
-
-    return command + ', '.join(ips)
 
 
 class SawtoothContainer:
@@ -70,44 +54,87 @@ class SawtoothContainer:
     def __del__(self):
         self.__client.close()
 
+    # starts a sawtooth container (peer) without starting PBFT
+    # this is needed because each peer must generate there keys before PBFT starts
+    # in addition when adding a new peer it has to catch up to the others first
+    # i.e. sync it's blockchain to the others before starting PBFT
     def start_instance(self):
-        container = self.__client.containers.run('sawtooth:final', detach=True)
-        run_command(container, 'sawtooth keygen')
-        run_command(container, 'sawadm keygen')
-        self.__container = container
-        self.__ip_addr = run_command(container, 'hostname -i')
-        self.__key = run_command(self.__container, 'cat /etc/sawtooth/keys/validator.pub')
+        self.__container = self.__client.containers.run('sawtooth:final', detach=True)
+        self.run_command('sawtooth keygen')
+        self.run_command('sawadm keygen')
+        self.__ip_addr = self.run_command('hostname -i')
+        self.__key = self.run_command('cat /etc/sawtooth/keys/validator.pub')
 
+    # makes a new genesis block, runs on one and only one peer in a committee
     def make_genesis(self, neighbours: list):
-        run_command(self.__container, SAWTOOTH_GENESIS_COMMANDS["genesis"])
+        self.run_command(SAWTOOTH_GENESIS_COMMANDS["genesis"])
 
         config_command = append_keys(neighbours, SAWTOOTH_GENESIS_COMMANDS["consensus_config"])
-        run_command(self.__container, config_command)
+        self.run_command(config_command)
 
         config_command = append_keys(neighbours, SAWTOOTH_GENESIS_COMMANDS["pbft_config"])
-        run_command(self.__container, config_command)
+        self.run_command(config_command)
 
-        run_command(self.__container, SAWTOOTH_GENESIS_COMMANDS["make_genesis"])
+        self.run_command(SAWTOOTH_GENESIS_COMMANDS["make_genesis"])
 
+    # starts PBFT
     def start_sawtooth(self, neighbours: list):
-        start_validator = append_endpoints(neighbours, SAWTOOTH_START_COMMANDS["validator"]) + '&'
-        run_command(self.__container, start_validator)
+        start_validator = self.__append_endpoints(neighbours, SAWTOOTH_START_COMMANDS["validator"])
+        start_validator = start_validator.replace('__IP__', self.ip())
+        self.run_service(start_validator)
 
-        run_command(self.__container, SAWTOOTH_START_COMMANDS["api"])
-        run_command(self.__container, SAWTOOTH_START_COMMANDS["transaction_processor"])
-        run_command(self.__container, SAWTOOTH_START_COMMANDS["client"])
-        run_command(self.__container, SAWTOOTH_START_COMMANDS["pbft"])
+        self.run_service(SAWTOOTH_START_COMMANDS["api"])
+        self.run_service(SAWTOOTH_START_COMMANDS["transaction_processor"])
+        self.run_service(SAWTOOTH_START_COMMANDS["client"])
+        self.run_service(SAWTOOTH_START_COMMANDS["pbft"].replace('__IP__', self.ip()))
 
+    # crashes  a peer
     def stop_instance(self):
-        self.__container.stop()
+        self.__container.stop(timeout=0)
 
+    # gets this peers unique key used for signing blocks
     def key(self):
         return self.__key
 
+    # gets this peers id
     def id(self):
         if self.__container is None:
             return None
         return self.__container.id
 
+    # gets this peers ip address
+    # all peers communicate via a virtual network hosted by docker. Docker runs DHCP and will assign each peer a new IP
+    # peers can access other peers by there ip address and only there ip address, there is no DNS
     def ip(self):
         return self.__ip_addr
+
+    # returns all currently running process in this peer
+    def top(self):
+        return self.__container.top()
+
+    # run a command in a container, will return the output of the command
+    def run_command(self, command: str):
+        return self.__container.exec_run(command).output.decode('utf-8').strip()
+
+    # start a service inside the container, will not return any output
+    def run_service(self, service_start_command: str):
+        self.__container.exec_run(service_start_command, detach=True)
+
+    # gets some json from the peer via a URL (ex: http://localhost:8008/blocks)
+    def sawtooth_api(self, request: str):
+        command = 'curl -sS {}'.format(request)
+        result = self.run_command(command)
+        return json.loads(result)
+
+    # takes a list of containers and a command appends the endpoints of each sawtooth instance to the command
+    # endpoints are added in the form [tcp://172.17.0.2:8800,tcp://172.17.0.3:8800, ...]
+    # will skip this peer
+    def __append_endpoints(self, containers, command):
+        ips = []
+        for c in containers:
+            if c.ip != self.__ip_addr:
+                ips.append(c.ip())
+        for i in range(len(ips)):
+            ips[i] = "tcp://{}:8800".format(ips[i])
+
+        return command + ', '.join(ips)
--- test/test_sawtooth.py
+++ test/test_sawtooth.py
@@ -1,14 +1,15 @@
 import unittest
+import time
 import docker as dockerapi
 from src.SawtoothPBFT import SawtoothContainer
-from src.SawtoothPBFT import run_command
 
 
-def KILL_ALL_CONTAINERS():
+def stop_all_containers():
     docker = dockerapi.from_env()
     for c in docker.containers.list():
         c.stop()
     docker.close()
+
 
 # gets a list of all running container ids
 def get_container_ids():
@@ -18,6 +19,7 @@ def get_container_ids():
         ids.append(c.id)
     docker.close()
     return ids
+
 
 class TestSawtoothMethods(unittest.TestCase):
 
@@ -29,44 +31,44 @@ class TestSawtoothMethods(unittest.TestCase):
 
         # test that container is made empty
         sawtooth_instance = SawtoothContainer()
-        assert (sawtooth_instance.id() is None)
-        assert (sawtooth_instance.ip() is None)
-        assert (sawtooth_instance.key() is None)
+        self.assertIs(sawtooth_instance.id(), None)
+        self.assertIs(sawtooth_instance.ip(), None)
+        self.assertIs(sawtooth_instance.key(), None)
 
         # test that once an instance is started that it has an id, ip and key
         sawtooth_instance.start_instance()
-        assert (len(docker.containers.list()) == 1)
-        assert (sawtooth_instance.id() is not None)
-        assert (sawtooth_instance.ip() is not None)
-        assert (sawtooth_instance.key() is not None)
+        self.assertEqual(len(docker.containers.list()), 1)
+        self.assertIsNot(sawtooth_instance.id(), None)
+        self.assertIsNot(sawtooth_instance.ip(), None)
+        self.assertIsNot(sawtooth_instance.key(), None)
 
-        container_ip = run_command(docker.containers.list()[0], "hostname -i")
-        container_key = run_command(docker.containers.list()[0], "cat /etc/sawtooth/keys/validator.pub")
-        assert (sawtooth_instance.id() == docker.containers.list()[0].id)
-        assert (sawtooth_instance.ip() == container_ip)
-        assert (sawtooth_instance.key() == container_key)
+        container_ip = docker.containers.list()[0].exec_run("hostname -i").output.decode('utf-8').strip()
+        container_key = docker.containers.list()[0].exec_run("cat /etc/sawtooth/keys/validator.pub").output.decode('utf-8').strip()
+        self.assertEqual(sawtooth_instance.id(), docker.containers.list()[0].id)
+        self.assertEqual(sawtooth_instance.ip(), container_ip)
+        self.assertEqual(sawtooth_instance.key(), container_key)
         number_of_running_processes = len(docker.containers.list()[0].top()['Processes'][0])
         # should only be 2 processes bash and tail -f /dev/null
         # each process has 4 columns so 2*4 = 8
-        assert (number_of_running_processes == 8)
+        self.assertEqual(number_of_running_processes, 8)
 
         # test that containers are made unique
         sawtooth_instance_2nd = SawtoothContainer()
 
         # test that container is made empty
-        assert (sawtooth_instance_2nd.id() is None)
-        assert (sawtooth_instance_2nd.ip() is None)
-        assert (sawtooth_instance_2nd.key() is None)
+        self.assertIs(sawtooth_instance_2nd.id(), None)
+        self.assertIs(sawtooth_instance_2nd.ip(), None)
+        self.assertIs(sawtooth_instance_2nd.key(), None)
 
         sawtooth_instance_2nd.start_instance()
-        assert (len(docker.containers.list()) == 2)
+        self.assertEqual(len(docker.containers.list()), 2)
         # tests that the two instance to not have the same IP or Key
-        assert (sawtooth_instance.id() != sawtooth_instance_2nd.id())
-        assert (sawtooth_instance.ip() != sawtooth_instance_2nd.ip())
-        assert (sawtooth_instance.key() != sawtooth_instance_2nd.key())
+        self.assertNotEqual(sawtooth_instance.id(), sawtooth_instance_2nd.id())
+        self.assertNotEqual(sawtooth_instance.ip(), sawtooth_instance_2nd.ip())
+        self.assertNotEqual(sawtooth_instance.key(), sawtooth_instance_2nd.key())
 
         # clean up
-        KILL_ALL_CONTAINERS()
+        stop_all_containers()
         docker.close()
 
     def test_kill_container(self):
@@ -74,31 +76,92 @@ class TestSawtoothMethods(unittest.TestCase):
         if len(docker.containers.list()) is not 0:
             self.skipTest("There should be no docker containers currently running, there was {} found.\n"
                           "Run \"docker ps\" to see all running containers".format(len(docker.containers.list())))
+
         sawtooth_instance = SawtoothContainer()
         sawtooth_instance.start_instance()
-        assert (len(docker.containers.list()) == 1)
-        assert (sawtooth_instance.id() in get_container_ids())
+        self.assertEqual(len(docker.containers.list()), 1)
+        self.assertIn(sawtooth_instance.id(), get_container_ids())
 
         sawtooth_instance_2nd = SawtoothContainer()
         sawtooth_instance_2nd.start_instance()
-        assert (len(docker.containers.list()) == 2)
-        assert (sawtooth_instance.id() in get_container_ids())
-        assert (sawtooth_instance_2nd.id() in get_container_ids())
+        self.assertEqual(len(docker.containers.list()), 2)
+        self.assertIn(sawtooth_instance.id(), get_container_ids())
+        self.assertIn(sawtooth_instance_2nd.id(), get_container_ids())
 
         # test that if one instance is stop only one instance stops
         sawtooth_instance.stop_instance()
-        assert (len(docker.containers.list()) == 1)
-        assert (sawtooth_instance_2nd.id() in get_container_ids())
+        self.assertEqual(len(docker.containers.list()), 1)
+        self.assertIn(sawtooth_instance_2nd.id(), get_container_ids())
 
         sawtooth_instance_2nd.stop_instance()
-        assert (len(docker.containers.list()) == 0)
+        self.assertEqual(len(docker.containers.list()), 0)
 
         # clean up
-        KILL_ALL_CONTAINERS()
+        stop_all_containers()
         docker.close()
 
     def test_committee_init_setup(self):
-        print("test_committee_setup")
+        docker = dockerapi.from_env()
+        if len(docker.containers.list()) is not 0:
+            self.skipTest("There should be no docker containers currently running, there was {} found.\n"
+                          "Run \"docker ps\" to see all running containers".format(len(docker.containers.list())))
+
+        # a committee needs a min of 4 members. Any less peers can not join or leave cleanly and they can not confirm
+        # new transactions
+        peers = [None, None, None, None]
+        for i in range(len(peers)):
+            peers[i] = SawtoothContainer()
+            peers[i].start_instance()
+
+        # make sure all containers have started
+        self.assertEqual(len(docker.containers.list()), 4)
+
+        peers[0].make_genesis(peers)
+        for p in peers:
+            p.start_sawtooth(peers)
+
+        # make sure all peers are running
+        for p in peers:
+            self.assertIn('sawtooth-validator',p.top()['Processes'][2][-1])
+            self.assertIn('/usr/bin/python3 /usr/bin/sawtooth-rest-api -v', p.top()['Processes'][3][-1])
+            self.assertIn('settings-tp -v', p.top()['Processes'][4][-1])
+            self.assertIn('/usr/bin/python3 /usr/bin/intkey-tp-python -v', p.top()['Processes'][5][-1])
+            self.assertIn('pbft-engine -vv --connect', p.top()['Processes'][6][-1])
+
+        # makes sure genesis block is in each peer
+        for p in peers:
+            blocks = p.sawtooth_api('http://localhost:8008/blocks')['data']
+            self.assertEqual(len(blocks), 1)
+
+        # makes sure all peers are configured to work with each other (this is not a test of connectivity just config)
+        ips = [p.ip() for p in peers]
+        for p in peers:
+            peers_config =  p.sawtooth_api('http://localhost:8008/peers')['data']
+            for ip in ips:
+                self.assertIn("tcp://{}:8800".format(ip), peers_config)
+
+        # test transaction confirmation
+        number_of_tx = 1
+        for p in peers:
+            p.run_command('intkey set test{} 999'.format(number_of_tx))
+            number_of_tx += 1
+            time.sleep(2)  # make sure TX has time to be confirmed
+            blockchain_size = len(p.sawtooth_api('http://localhost:8008/blocks')['data'])
+            self.assertEqual(blockchain_size, number_of_tx)
+
+        # test that committee breaks if members drop below 4
+        peers[0].stop_instance()
+        peers.pop(0)
+
+        blockchain_size = len(peers[1].sawtooth_api('http://localhost:8008/blocks')['data'])
+        for p in peers:
+            p.run_command('intkey set fail 000')
+            time.sleep(2)
+            peers_blockchain = len(p.sawtooth_api('http://localhost:8008/blocks')['data'])
+            self.assertEqual(peers_blockchain, blockchain_size)
+
+        stop_all_containers()
+        docker.close()
 
     def test_peer_join(self):
         print("test_join")
