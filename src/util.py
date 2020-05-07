8b5f4cea70a12b72c770f3d19342fd21015ad961
Kendric Hood <khood5@kent.edu>
2020-05-07 13:48:30 -0400

    working my way though additions/corrections to tests and peer module. Fixed some typos and added some getter functions (allows for better logging). Added some tests got init committee setup and transactions processing running. Join works, adding tests for committee independence. Have not gotten to removing a peer.

--- src/util.py
+++ src/util.py
@@ -1,5 +1,6 @@
 import docker as dockerapi
 from src.SawtoothPBFT import SawtoothContainer
+from src.Peer import Peer
 
 
 def stop_all_containers():
@@ -33,5 +34,14 @@ def make_sawtooth_committee(size: int):
         for p in peers:
             if len(p.blocks()['data']) != 1:
                 done = False
+
+    return peers
+
+
+# makes 2 quorums each with size number of peers (with whole committee intersection i.e. each peer is in both quorums)
+def make_peer_committees(size: int, id_a=1, id_b=2):
+    containers_a = make_sawtooth_committee(size)
+    containers_b = make_sawtooth_committee(size)
+    peers = [Peer(containers_a[i], containers_b[i], id_a, id_b) for i in range(size)]
 
     return peers
