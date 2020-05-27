c9961da7f4ce2d6378e5c779b19ab8b7a1af9c65
Kendric Hood <khood5@kent.edu>
2020-05-27 18:17:52 -0400

    fixed issue with too many files being open at once

--- src/SawtoothPBFT.py
+++ src/SawtoothPBFT.py
@@ -92,11 +92,12 @@ def append_keys(keys, command):
 
 
 class SawtoothContainer:
+    __client = docker.from_env()
 
     # starts a sawtooth container and generates root and validator keys
     # does not start PBFT
     def __init__(self):
-        self.__client = docker.from_env()
+        # self.__client = docker.from_env()
         self.__container = self.__client.containers.run(DOCKER_IMAGE, detach=True)
         self.__ip_addr = self.run_command('hostname -i')
         self.run_command('sawtooth keygen')
