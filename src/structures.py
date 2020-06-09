f99f87aab17899f6946b275d34233c196ab68bc6
Kendric Hood <khood5@kent.edu>
2020-06-09 01:35:35 -0400

    made some adjustments to Transaction. Now it can convert to JSON and a tx can be load from JSON, Tested and works

--- src/structures.py
+++ src/structures.py
@@ -1,8 +1,11 @@
+from src.api.constants import TRANSACTION_KEY, TRANSACTION_VALUE, QUORUM_ID
+import json
+
+
 class Transaction:
-    def __init__(self, quorum, tx=0):
+    def __init__(self, quorum: str):
         self.quorum_id = quorum
-        self.tx_number = tx
-        self.key = "{}".format(tx)
+        self.key = ""
         self.value = ""
 
     def id(self):
@@ -11,7 +14,12 @@ class Transaction:
         # confirming the tx
         return self.quorum_id
 
-    def sequence_number(self):
-        # this is the sequence number of the transaction
-        # no two transactions should ever has the same sequence number
-        return self.tx_number
+    def to_json(self):
+        return json.loads(json.dumps({QUORUM_ID: self.quorum_id,
+                                      TRANSACTION_KEY: self.key,
+                                      TRANSACTION_VALUE: self.value}))
+
+    def load_from_json(self, json_data):
+        self.quorum_id = json_data[QUORUM_ID]
+        self.key = json_data[TRANSACTION_KEY]
+        self.value = json_data[TRANSACTION_VALUE]
---
+++ test/test_structures.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+from src.structures import Transaction
+
+
+class TestPeerMethods(unittest.TestCase):
+
+    def test_transaction_to_json(self):
+        tx = Transaction("a")
+        tx.key = "test"
+        tx.value = "999"
+        json_data = {"quorum_id": "a", "key": "test", "value": "999"}
+        result = tx.to_json()
+        self.assertEqual(json_data["quorum_id"], result["quorum_id"])
+        self.assertEqual(json_data["key"], result["key"])
+        self.assertEqual(json_data["value"], result["value"])
+
+    def test_transaction_from_json(self):
+        json_data = json.loads(json.dumps({"quorum_id": "b", "key": "test2", "value": "888"}))
+        tx = Transaction("a")
+        tx.load_from_json(json_data)
+        valid_tx = Transaction("b")
+        valid_tx.key = "test2"
+        valid_tx.value = "888"
+        self.assertEqual(valid_tx.quorum_id, tx.quorum_id)
+        self.assertEqual(valid_tx.key, tx.key)
+        self.assertEqual(valid_tx.value, tx.value)
