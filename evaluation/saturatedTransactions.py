bbc7b2543635bf4fd0bce02f738fe6444cfec74b
WolfWindOW <55042320+WolfWindOW@users.noreply.github.com>
2021-08-31 15:41:27 -0400

    Changes to tx submission, range is now multiplicative

--- evaluation/saturatedTransactions.py
+++ evaluation/saturatedTransactions.py
@@ -13,11 +13,11 @@ from src.structures import Transaction
 from src.util import make_intersecting_committees_on_host
 
 #Defaults
-NUMBER_OF_TX_MIN = 6
-NUMBER_OF_TX_MAX = 12
+NUMBER_OF_TX_MULT = 3
+NUMBER_OF_TX = 12
 NUMBER_OF_COMMITTEES = 2
 INTERSECTION = 7
-NUMBER_OF_EXPERIMENTS = 1
+NUMBER_OF_EXPERIMENTS = 3
 OUTPUT_FILE = "TransactionSaturation.csv"
 
 #Const
@@ -27,18 +27,19 @@ URL_HOST = "http://{ip}:{port}"
 
 
 #Opens output file and writes results in it for each data point
-def make_graph_data(outfile: str, min: int, max: int, number_of_intersections: int, experiments: int, committees: int):
+def make_graph_data(outfile: str, max: int, number_of_intersections: int, experiments: int, committees: int):
     out = open(outfile, 'w')
     print("Outputting to {}".format(outfile))
-    out.write("Number of transactions submitted, throughput\n")
-    #For each number of transaction
-    for number_of_transactions in range(NUMBER_OF_TX_MIN, NUMBER_OF_TX_MAX + 1):
+    m = 1
+    while m < NUMBER_OF_TX_MULT:
+        out.write("Number of transactions submitted, throughput\n")
         print("----------------------------------------------------------")
-        print("Starting experiments for transaction amount {}".format(number_of_transactions))
-        avgs = get_avg_for(number_of_transactions, number_of_intersections, experiments, committees)
-        print("Experiments for transaction amount {} ended".format(number_of_transactions))
+        print("Starting experiments for transaction amount {}".format(max*m))
+        avgs = get_avg_for((max*m), number_of_intersections, experiments, committees)
+        print("Experiments for transaction amount {} ended".format(max*m))
         print("----------------------------------------------------------")
-        out.write("{s}, {w}\n".format(s=number_of_transactions, w=avgs["throughput"]))
+        out.write("{s}, {w}\n".format(s=(max*m), w=avgs["throughput"]))
+        m += 1
     out.close()
 
 
@@ -66,81 +67,48 @@ def get_avg_for(number_of_transactions: int, number_of_intersections: int, exper
 #Gets the individual data for each amount of transactions sent
 def run_experiment(peers: dict, number_of_transactions: int):
     print("Running", end='', flush=True)
-    submitted_tx, totalSubmitted, amount_of_confirmedtx_per_5sec, amount_of_submittedtx_per_5sec, confirmedTXs, \
-    number_of_submitted_tx  = {}, 0, [], [], [], 0
-    startSubFlag = 0
+    submitted_tx, amount_of_confirmedtx_per_5sec, confirmedTXs, urls = {}, [], [], []
     committee_ids = [peers[p].committee_id_a() for p in peers]
     committee_ids.extend([peers[p].committee_id_b() for p in peers])
     committee_ids = list(dict.fromkeys(committee_ids))
     peerList = list(peers.keys())
     peerQuorum = peersByQuorum(committee_ids, peers)
-    #next_sub = time.time() + SUBTIME
-    #While there are still transactions to be made
-    while totalSubmitted < number_of_transactions:
-        if 1:
-            #To allow many submitted in a given time swapped to always true
-            #time.time() > next_sub:
-            #Gets localtime
-            currentTime = time.localtime(time.time())
-            #Every 10 seconds starts a new time slot and if submissions have started
-            if currentTime.tm_sec % 10 == 0 and startSubFlag == 1:
-                amount_of_submittedtx_per_5sec.append(number_of_submitted_tx)
-                number_of_submitted_tx = 0
-                amount_of_confirmedtx_per_5sec.append(len(confirmedTXs))
-                confirmedTXs.clear()
-            print("Submitting tx {}".format(totalSubmitted))
-            tx = Transaction(quorum=choice(committee_ids), key="tx_{}".format(totalSubmitted), value="{}".format(999))
-            number_of_submitted_tx += 1
-            totalSubmitted += 1
-            submitted_tx[time.time()] = tx
-            peerSelected = choice(peerList)
-            startSubFlag = 1
-            #Forces the transaction to go to a different committee than the one selected
-            #FIX KEYERROR for forced
-            #KeyError eg. '4'. likely a problem with directly comparing committee ids and the quorum ids
-            #while peers[peerSelected].committee_id_a() == tx.quorum_id or ...committee_id_b() == tx.quorum_id:
-            #    peerSelected = choice(committee_ids)
-            url = URL_HOST.format(ip=IP_ADDRESS, port=peerSelected) + "/submit/"
-            requests.post(url, json=tx.to_json())
-            #Every 30 seconds places a period in console
-            #if time.time() % 30:
-                #print(" .", end='', flush=True)
-            check_submitted_tx(confirmedTXs, submitted_tx, peerQuorum)
-    time.sleep(1)
+    m = 0
+    while m < number_of_transactions:
+        print("Submitting tx {}".format(m))
+        tx = Transaction(quorum=choice(committee_ids), key="tx_{}".format(m), value="{}".format(999))
+        m += 1
+        submitted_tx[time.time()] = tx
+        peerSelected = choice(peerList)
+        url = URL_HOST.format(ip=IP_ADDRESS, port=peerSelected) + "/submit/"
+        urls.append(url)
+        #check_submitted_tx(confirmedTXs, submitted_tx, peerQuorum)
+    for url in urls:
+        requests.post(url, json=tx.to_json())
+    #time.sleep(1)
     check_submitted_tx(confirmedTXs, submitted_tx, peerQuorum)
+    amount_of_confirmedtx_per_5sec.append(len(confirmedTXs))
     #ensures that all submitted and confirmed transactions are added to list
-    if number_of_submitted_tx != 0 or len(confirmedTXs) != 0:
-        amount_of_submittedtx_per_5sec.append(number_of_submitted_tx)
-        number_of_submitted_tx = 0
-        amount_of_confirmedtx_per_5sec.append(len(confirmedTXs))
-        confirmedTXs.clear()
-    print()
+    #if number_of_submitted_tx != 0 or len(confirmedTXs) != 0:
+        #amount_of_submittedtx_per_5sec.append(number_of_submitted_tx)
+        #number_of_submitted_tx = 0
+        #amount_of_confirmedtx_per_5sec.append(len(confirmedTXs))
+        #confirmedTXs.clear()
     throughputPer5 = []
     #Fix always displays 0 for first 5 seconds
     n = 0
     amountCommitted = 0
-    amountSubmitted = 0
+    #amountSubmitted = 0
     while n < len(amount_of_confirmedtx_per_5sec):
-    #for n in range(0, len(amount_of_confirmedtx_per_5sec)):
-        if amount_of_submittedtx_per_5sec[n] == 0:
-            #throughputPer5.append(0)
-            amountSubmitted += 0
-        else:
-            #print("Amount of confirmed per 10 sec: {}".format(amount_of_confirmedtx_per_5sec[n]))
-            #print("Amount of submitted per 10 sec: {}".format(amount_of_submittedtx_per_5sec[n]))
-            amountCommitted += amount_of_confirmedtx_per_5sec[n]
-            amountSubmitted += amount_of_submittedtx_per_5sec[n]
-            #throughputPer5.append(float(amount_of_confirmedtx_per_5sec[n]/amount_of_submittedtx_per_5sec[n]))
-            #print("Throughput per 10: {}".format(throughputPer5[n]))
+        amountCommitted += amount_of_confirmedtx_per_5sec[n]
         n += 1
-    throughputPer5.append(float(amountCommitted/amountSubmitted))
+    throughputPer5.append(float(amountCommitted/number_of_transactions))
     return {"throughput": throughputPer5}
 
 def check_submitted_tx(confirmed, sub, port):
     remove_from_sub = []
     for tx in sub:
         url = URL_HOST.format(ip=IP_ADDRESS, port=choice(port[sub[tx].quorum_id]) + "/get/")
-        time.sleep(.1)
         if sub[tx].value == get_plain_text(requests.post(url, json=sub[tx].to_json())):
             confirmed.append(tx)
             remove_from_sub.append(tx)
@@ -160,8 +128,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make a performance graph for forward based on transaction amount. '
                                                  'Each data point is an amount of transactions. Runs from tx#=q to tx#=2p')
     parser.add_argument('-o', type=str, help='File to output data (csv format)')
-    parser.add_argument('-min', type=int, help='Starting number of transactions. Default {}'.format(NUMBER_OF_TX_MIN))
-    parser.add_argument('-max', type=int, help='Max number of transactions. Default {}'.format(NUMBER_OF_TX_MAX))
+    #parser.add_argument('-min', type=int, help='Starting number of transactions. Default {}'.format(NUMBER_OF_TX_MIN))
+    parser.add_argument('-max', type=int, help='Max number of transactions. Default {}'.format(NUMBER_OF_TX))
     parser.add_argument('-i', type=int, help='Intersection between committees. Default {}'.format(INTERSECTION))
     parser.add_argument('-e', type=int, help='Number of experiments to run per data point. '
                                              'Default {}'.format(NUMBER_OF_EXPERIMENTS))
@@ -174,13 +142,12 @@ if __name__ == '__main__':
     
     experiments = NUMBER_OF_EXPERIMENTS if args.e is None else args.e
     committees = NUMBER_OF_COMMITTEES if args.t is None else args.t
-    starting_number_of_transactions = NUMBER_OF_TX_MIN if args.min is None else args.min
-    ending_number_of_transactions = NUMBER_OF_TX_MAX if args.max is None else args.max
+    #starting_number_of_transactions = NUMBER_OF_TX_MIN if args.min is None else args.min
+    number_of_transactions = NUMBER_OF_TX if args.max is None else args.max
     number_of_intersections = INTERSECTION if args.i is None else args.i
     sawtooth_container_log_to(Path().cwd().joinpath('{}.SawtoothContainer.log'.format(__file__)))
     intersection_log_to(Path().cwd().joinpath('{}.Intersection.log'.format(__file__)))
     smart_shard_peer_log_to(Path().cwd().joinpath('{}.SmartShardPeer.log'.format(__file__)))
     print("experiments: {e}, committees: {c}".format(e=experiments, c=committees))
 
-    make_graph_data(output_file, starting_number_of_transactions, ending_number_of_transactions,
-                    number_of_intersections, experiments, committees)
+    make_graph_data(output_file, number_of_transactions, number_of_intersections, experiments, committees)
