dbaacabd45e6e0e8d86125db9abc3a00ecfb1248
WolfWindOW <55042320+WolfWindOW@users.noreply.github.com>
2021-10-19 19:51:52 -0400

    Cleaned up comments, unique keys, checks txs direct from peers

--- evaluation/saturatedTransactions.py
+++ evaluation/saturatedTransactions.py
@@ -17,10 +17,10 @@ from src.util import make_intersecting_committees_on_host
 
 # Defaults
 NUMBER_OF_TX_MULT = 2
-NUMBER_OF_TX = 8
+NUMBER_OF_TX = 7
 NUMBER_OF_COMMITTEES = 2
 INTERSECTION = 7
-NUMBER_OF_EXPERIMENTS = 3
+NUMBER_OF_EXPERIMENTS = 5
 OUTPUT_FILE = "TransactionSaturation.csv"
 
 # Const
@@ -65,7 +65,7 @@ def get_avg_for(number_of_transactions: int, number_of_intersections: int, exper
             throughputPer5[e] = 0
         # takes the amount of transactions confirmed per experiment and divides them by the runs in each experiment
         else:
-            throughputPer5[e] = sum(throughputPer5[e])/len(throughputPer5[e])
+            throughputPer5[e] = sum(throughputPer5[e]) / len(throughputPer5[e])
     for e in throughputPer5:
         throughput.append(throughputPer5[e])
     return {"throughput": sum(throughput) / len(throughput)}
@@ -74,7 +74,7 @@ def get_avg_for(number_of_transactions: int, number_of_intersections: int, exper
 # Gets the individual data for each amount of transactions sent
 def run_experiment(peers: dict, number_of_transactions: int):
     print("Running", end='', flush=True)
-    submitted_tx, amount_of_confirmedtx_per_5sec, confirmedTXs = {}, [], []
+    submitted_tx, confirmedTXs, yetToBeConfirmedTXs = {}, {}, {}
     committee_ids = [peers[p].committee_id_a() for p in peers]
     committee_ids.extend([peers[p].committee_id_b() for p in peers])
     committee_ids = list(dict.fromkeys(committee_ids))
@@ -82,26 +82,19 @@ def run_experiment(peers: dict, number_of_transactions: int):
     peerQuorum = peersByQuorum(committee_ids, peers)
     urlTXTuplesList = []
     submittedTxList = []
-    # peerQuorumList = peersByQuorumList(peerQuorum)
-    m = 0
     # Amount of individual runs per experiment
     seconds = 30
     # Creates the amount of groups of tx equal to the amount of runs
-    while m < seconds:
-        createTXs(number_of_transactions, urlTXTuplesList, committee_ids, submittedTxList, peerList, peerQuorum)
-        m += 1
-    o = 0
-    # Creates a list of tuples of urls and json
-    # Use if on line 2
-    urlJsonList = []
-    for e in submittedTxList:
-        urlJsonList.append(createUrlJsonList(e))
+    createTXs(number_of_transactions, urlTXTuplesList, committee_ids, submittedTxList, peerList, peerQuorum, seconds)
+    round = 0
+    submittedtxNumber = 0
     # Runs for the amount of runs
     startTime = time.time()
     # Grab start time Current minus start
     # if > seconds
     while (time.time() - startTime) < seconds:
-        o += 1
+        round += 1
+        submittedtxNumber = number_of_transactions * round
         # Creates multiprocessing pool
         pool = ThreadPool(number_of_transactions)
         # Divides the task into the pool
@@ -109,29 +102,62 @@ def run_experiment(peers: dict, number_of_transactions: int):
         # Processes and rejoins the pool
         pool.close()
         pool.join()
-        # Checks the transactions
-        check_submitted_tx(confirmedTXs, submittedTxList[0], urlJsonList[0])
-        # Removes top of urlJsonList
-        urlJsonList.pop(0)
-        # Removes the group of submitted txs
-        submittedTxList.pop(0)
         # Removes the top element from the groups of txs
         urlTXTuplesList.pop(0)
         # If less than 1 second has passed, sleep for the difference
-        if (time.time() - startTime) < o:
-            time.sleep(o - (time.time() - startTime))
-    amount_of_confirmedtx_per_5sec.append((len(confirmedTXs))/(seconds-len(urlTXTuplesList)))
-    throughputPer5 = []
-    n = 0
-    amountCommitted = 0
-    while n < len(amount_of_confirmedtx_per_5sec):
-        amountCommitted += amount_of_confirmedtx_per_5sec[n]
-        n += 1
-    throughputPer5.append(float(amountCommitted/number_of_transactions))
-    return {"throughput": throughputPer5}
+        if (time.time() - startTime) < round:
+            time.sleep(round - (time.time() - startTime))
+    for i in range(round):
+        check_from_peers(submittedTxList[i], confirmedTXs, peers, peerList)
+    throughput = float(len(confirmedTXs) / submittedtxNumber)
+    throughputPerExperiment = []
+    throughputPerExperiment.append(throughput)
+    return {"throughput": throughputPerExperiment}
 
 
-def check_submitted_tx(confirmed, sub, urlJsonList):
+def check_from_peers(submitted, confirmed, peers, peerList):
+    intersectionA = peers[peerList[0]].inter
+    for tx in submitted:
+        if intersectionA.get_tx(tx[1]) == tx[1].value:
+            confirmed[tx] = tx
+
+
+def check_submitted_tx(confirmed, sub, urlJsonList, startTime, timeEnd, unconfirmed, yetToBeConfirmedTXs):
+    remove_from_sub = []
+    notyetconfirmedtxs = []
+    txsText = []
+    pool = ThreadPool(number_of_transactions)
+    txsText.append(pool.map(getTxs, urlJsonList))
+    pool.close()
+    pool.join()
+    i = 0
+    # Checks all the submitted transactions for this round
+    for tx in sub:
+        # Only continues while the time hasnt been exceeded
+        if (time.time() - startTime) < timeEnd:
+            if tx[1].value == txsText[0][i]:
+                confirmed.append(tx)
+                remove_from_sub.append(tx)
+            i = i + 1
+    # Removes all the confirmed txs from submitted
+    for r in remove_from_sub:
+        i = 0
+        for tx in sub:
+            if tx == r:
+                del sub[i]
+            i = i + 1
+    # moves all of the unconfirmed to yettobe
+    for r in sub:
+        notyetconfirmedtxs.append(r)
+    for tx in yetToBeConfirmedTXs:
+        url = URL_HOST.format(ip=IP_ADDRESS, port=tx[2] + "/get/")
+        jsonTxt = tx[1].to_json()
+        unconfirmed.append(url, jsonTxt)
+    for r in notyetconfirmedtxs:
+        yetToBeConfirmedTXs.append(r)
+
+
+def check_unconfirmed_tx(confirmed, urlJsonList, startTime, timeEnd, yetToBeConfirmedTXs):
     remove_from_sub = []
     txsText = []
     pool = ThreadPool(number_of_transactions)
@@ -139,11 +165,14 @@ def check_submitted_tx(confirmed, sub, urlJsonList):
     pool.close()
     pool.join()
     i = 0
-    for tx in sub:
-        if tx[1].value == txsText[0][i]:
-            confirmed.append(tx)
-            remove_from_sub.append(tx)
-        i = i + 1
+    # Checks all the submitted transactions for this round
+    for tx in yetToBeConfirmedTXs:
+        # Only continues while the time hasnt been exceeded
+        if (time.time() - startTime) < timeEnd:
+            if tx[1].value == txsText[0][i]:
+                confirmed.append(tx)
+                remove_from_sub.append(tx)
+            i = i + 1
 
 
 # Creates a dictionary of quorums, which has the peer/ports of the quorum as values
@@ -171,15 +200,14 @@ def peersByQuorumList(dict):
     return peerQuorumList
 
 
-# Round robin this
 # Creates a grouping of tuples for the transactions. Time, tx, peer
-def createTXs(txNumber, listOfTuples, committee_ids, submittedTxList, peerList, peerQuorumList):
+def createTXs(txNumber, listOfTuples, committee_ids, submittedTxList, peerList, peerQuorumList, seconds):
     n = 0
+    peerSelected = deque(peerList)
     submittedTx = []
     urlTXTuples = []
-    peerSelected = deque(peerList)
     # Creates a group of transactions equal to the txNumber
-    while n < txNumber:
+    while n < (txNumber*seconds):
         tx = Transaction(quorum=choice(committee_ids), key="tx_{}".format(n), value="{}".format(999))
         selectedPeer = choice(peerQuorumList[tx.quorum_id])
         submittedTx.append((time.time(), tx, selectedPeer))
@@ -188,10 +216,11 @@ def createTXs(txNumber, listOfTuples, committee_ids, submittedTxList, peerList, 
         n += 1
         poppedPeer = peerSelected.popleft()
         peerSelected.append(poppedPeer)
-    # Adds the txs to a list of groups
-    listOfTuples.append(urlTXTuples)
-    # Also adds the txs to a list of dictionaries of txs
-    submittedTxList.append(submittedTx)
+        if (n != 0) & (n % txNumber == 0):
+            listOfTuples.append(urlTXTuples)
+            submittedTxList.append(submittedTx)
+            submittedTx = []
+            urlTXTuples = []
 
 
 # Submits each of the transactions, removes the used grouping from the list
@@ -200,9 +229,9 @@ def submitTxs(tuplesList):
     requests.post(tuplesList[0], json=tuplesList[1].to_json())
 
 
-#Gets the transactions
+# Gets the transactions
 def getTxs(urlJsonList):
-    return get_plain_text(requests.post(urlJsonList[0], json=urlJsonList[1]))
+    return {urlJsonList[0]: get_plain_text(requests.post(urlJsonList[0], json=urlJsonList[1]))}
 
 
 def createUrlJsonList(sub):
@@ -212,7 +241,6 @@ def createUrlJsonList(sub):
         jsonTxt = tx[1].to_json()
         urlJsonList.append((url, jsonTxt))
     return urlJsonList
-
 
 
 if __name__ == '__main__':
@@ -229,7 +257,7 @@ if __name__ == '__main__':
     output_file = Path(args.o) if args.o is not None else Path().cwd().joinpath(OUTPUT_FILE)
     while not output_file.exists():
         output_file.touch()
-    
+
     experiments = NUMBER_OF_EXPERIMENTS if args.e is None else args.e
     committees = NUMBER_OF_COMMITTEES if args.t is None else args.t
     number_of_transactions = NUMBER_OF_TX if args.max is None else args.max
