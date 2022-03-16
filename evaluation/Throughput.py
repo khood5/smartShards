50a8ea34134804cb84a625ffc3eef389c3d24184
WolfWindOW <55042320+WolfWindOW@users.noreply.github.com>
2022-03-15 22:57:11 -0400

    Added basic throughput evaluation as well as log parser, enhanced from timing parser.
    Throughput eval only checks at the end, rather than throughout the experiment

---
+++ evaluation/Throughput.py
@@ -0,0 +1,198 @@
+import argparse
+import gc
+import time
+from math import floor
+from pathlib import Path
+from random import choice
+
+import requests
+from src.Intersection import intersection_log_to
+from src.SawtoothPBFT import sawtooth_container_log_to
+from src.SmartShardPeer import smart_shard_peer_log_to
+from src.api.api_util import get_plain_text
+from src.structures import Transaction
+from src.util import make_intersecting_committees_on_host
+
+
+# Quorums and Intersections to use
+# 3 -- 20  == 60
+# 4 -- 10 == 60
+# 5 -- 6 == 60
+# 6 -- 4 == 60
+# 7 -- 3 == 63
+# 8 -- 2 == 56
+# 11 -- 1 == 55
+# 12 -- 1 == 66
+
+# Defaults
+NUMBER_OF_TX = 20
+NUMBER_OF_COMMITTEES = 3
+INTERSECTION = 20
+NUMBER_OF_EXPERIMENTS = 10
+OUTPUT_FILE = "Throughput.csv"
+EXPERIMENT_DURATION_SECS = 300
+MEASUREMENT_INTERVAL = 5
+
+# Const
+IP_ADDRESS = "localhost"
+URL_HOST = "http://{ip}:{port}"
+
+# Opens output file and writes results in it for each data point
+def make_graph_data(outfile: Path, number_of_tx: int, experiment_duration_secs: int, measurement_interval_secs: int,
+                    number_of_intersections: int, experiments: int, committees: int):
+    out = open(outfile, 'w')
+    print("Outputting to {}".format(outfile))
+    out.write("Seconds for confirmation, Number of confirmed transactions\n")
+    print("----------------------------------------------------------")
+    print("Starting experiments for transaction amount {}".format(number_of_tx))
+    avgs = get_avg_for(number_of_tx, experiment_duration_secs, measurement_interval_secs, number_of_intersections,
+                           experiments, committees)
+    print("Experiments for transaction amount {} ended".format(number_of_tx))
+    print("----------------------------------------------------------")
+    out.write("{s}, {w}\n".format(s=number_of_tx, w=avgs["throughput"]))
+    out.close()
+
+
+# Run each experiment and calc avgs
+def get_avg_for(number_of_transactions: int, experiment_duration_secs: int, measurement_interval_secs: int,
+                number_of_intersections: int, experiments: int, committees: int):
+    throughputPerE = {}
+    for e in range(experiments):
+        print("Setting up experiment {}".format(e))
+        peers = make_intersecting_committees_on_host(committees, number_of_intersections)
+        results = run_experiment(peers, experiment_duration_secs, measurement_interval_secs, number_of_transactions)
+        throughputPerE[e] = results["throughput"]
+        print("Cleaning up experiment {}".format(e))
+        del peers
+        gc.collect()
+    throughput = {}
+    # For each experiment
+    for e in range(experiments):
+        # If no transactions were confirmed, inputs zero, to prevent dividing by zero
+        if len(throughputPerE[e]) == 0:
+            throughputPerE[e] = 0
+        # takes the amount of transactions confirmed per experiment and divides them by the runs in each experiment
+        else:
+            for key in throughputPerE[e]:
+                if key in throughput:
+                    value = throughput[key] + throughputPerE[e][key]
+                    throughput[key] = value
+                else:
+                    throughput[key] = throughputPerE[e][key]
+    sortedlist = sorted(throughput.items())
+    sortedThroughput = dict(sortedlist)
+    return {"throughput": sortedThroughput}
+
+
+# Gets the individual data for each amount of transactions sent
+# peers: dict of peers port number as key and SmartShardPeer as obj
+# experiment_duration_secs: how long to run experiment for
+# number_of_transactions: number of transactions per round (1 sec)
+def run_experiment(peers: dict, experiment_duration_secs: int, measurement_interval_secs: int,
+                   number_of_transactions: int):
+    print("Running", end='', flush=True)
+    committee_ids = [peers[p].committee_id_a() for p in peers]
+    committee_ids.extend([peers[p].committee_id_b() for p in peers])
+    committee_ids = list(dict.fromkeys(committee_ids))
+    unsubmitted_tx_by_round = []  # list of transactions that should be submitted per round
+    txTimeSubAndConf = []
+    # Creates the amount of groups of tx equal to the amount of runs
+    for round in range(0, experiment_duration_secs):
+        txTimeSubAndConf.append([])
+        unsubmitted_tx_by_round.append(create_txs(peers, committee_ids, number_of_transactions, round, txTimeSubAndConf))
+    round = 0
+    startTime = time.time()
+    while (time.time() - startTime) < experiment_duration_secs:
+        for tx in unsubmitted_tx_by_round[round]:
+            submitTxs(tx)
+        for tx in txTimeSubAndConf[round]:
+            tx.append(floor(time.time()))
+        # If less than 1 second has passed, sleep for the difference
+        if (time.time() - startTime) < round:
+            time.sleep(round - (time.time() - startTime))
+        round += 1
+    for totalRounds in range(0, round):
+        check_from_peers(unsubmitted_tx_by_round[totalRounds], txTimeSubAndConf[totalRounds], peers)
+    timeToConfirm = {}
+    for totalRounds in range(0, round):
+        for tx in range(0, number_of_transactions):
+            if len(txTimeSubAndConf[totalRounds][tx])>1:
+                if (floor(txTimeSubAndConf[totalRounds][tx][1])-floor(txTimeSubAndConf[totalRounds][tx][0])) in timeToConfirm:
+                    timeToConfirm[(floor(txTimeSubAndConf[totalRounds][tx][1])-floor(txTimeSubAndConf[totalRounds][tx][0]))] += 1
+                else:
+                    timeToConfirm[(floor(txTimeSubAndConf[totalRounds][tx][1]) - floor(txTimeSubAndConf[totalRounds][tx][0]))] = 1
+    return {"throughput": timeToConfirm}
+
+
+def check_from_peers(submitted, confirmed, peers):
+    url = URL_HOST.format(ip=IP_ADDRESS, port=str(list(peers.keys())[0]) + "/get/")
+    remove_from_sub = []
+    for tx in submitted:
+        if tx[1].value == get_plain_text(requests.post(url, json=tx[1].to_json())):
+            remove_from_sub.append(tx)
+            txID = (tx[1].key.split('_'))[2]
+            confirmed[int(txID)].append(floor(time.time()))
+    for r in remove_from_sub:
+        i = 0
+        for tx in submitted:
+            if r[1].key == tx[1].key:
+                del submitted[i]
+                break
+            i += 1
+
+
+# Submits each of the transactions, removes the used grouping from the list
+def submitTxs(tuplesList):
+    # Submits the transaction, using the url as a key
+    requests.post(tuplesList[0], json=tuplesList[1].to_json())
+
+
+def setTime(txTime):
+    txTime.append(floor(time.time()))
+
+
+def create_txs(peers, committee_ids, number_of_transactions, round, txTimeSubAndConf):
+    url_tx_tuples = []
+    peer_ports = list(peers.keys())
+    # Creates a group of transactions equal to the txNumber
+    for tx_id in range(0, number_of_transactions):
+        tx = Transaction(quorum=choice(committee_ids), key="tx_{round}_{id}".format(round=round, id=tx_id), value="{}".format(999))
+        selected_peer = choice(peer_ports)
+        url = URL_HOST.format(ip=IP_ADDRESS, port=selected_peer) + "/submit/"
+        url_tx_tuples.append((url, tx, time.time()))
+        peer_ports.append(peer_ports.pop(0))
+        txTimeSubAndConf[round].append(([]))
+    return url_tx_tuples
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Make a performance graph based on throughput per measurement interval. '
+                                                 'Each data point is an amount of transactions. Runs from tx#=q to tx#=2p')
+    parser.add_argument('-o', type=str, help='File to output data (csv format)')
+    parser.add_argument('-max', type=int,
+                        help='Max number of transactions per round (1 sec). Default {}'.format(NUMBER_OF_TX))
+    parser.add_argument('-d', type=int, help='experiment duration in secs. Default {}'.format(EXPERIMENT_DURATION_SECS))
+    parser.add_argument('-i', type=int, help='Intersection between committees. Default {}'.format(INTERSECTION))
+    parser.add_argument('-e', type=int, help='Number of experiments to run per data point. '
+                                             'Default {}'.format(NUMBER_OF_EXPERIMENTS))
+    parser.add_argument('-t', type=int, help='Total number of committees. Default {}'.format(NUMBER_OF_COMMITTEES))
+    parser.add_argument('-m', type=int, help='measurement interval in secs. Default {}'.format(MEASUREMENT_INTERVAL))
+    args = parser.parse_args()
+
+    output_file = Path(args.o) if args.o is not None else Path().cwd().joinpath(OUTPUT_FILE)
+    while not output_file.exists():
+        output_file.touch()
+
+    experiments = NUMBER_OF_EXPERIMENTS if args.e is None else args.e
+    committees = NUMBER_OF_COMMITTEES if args.t is None else args.t
+    number_of_transactions = NUMBER_OF_TX if args.max is None else args.max
+    experiment_duration_secs = EXPERIMENT_DURATION_SECS if args.d is None else args.d
+    measurement_interval_secs = MEASUREMENT_INTERVAL if args.m is None else args.m
+    number_of_intersections = INTERSECTION if args.i is None else args.i
+    sawtooth_container_log_to(Path().cwd().joinpath('{}.SawtoothContainer.log'.format(__file__)))
+    intersection_log_to(Path().cwd().joinpath('{}.Intersection.log'.format(__file__)))
+    smart_shard_peer_log_to(Path().cwd().joinpath('{}.SmartShardPeer.log'.format(__file__)))
+    print("experiments: {e}, committees: {c}".format(e=experiments, c=committees))
+
+    make_graph_data(output_file, number_of_transactions, experiment_duration_secs,
+                    measurement_interval_secs, number_of_intersections, experiments, committees)
---
+++ evaluation/throughputLogs.py
@@ -0,0 +1,52 @@
+import sys
+from datetime import datetime
+
+
+#Takes two times, of format HH:MM:SS and finds the difference
+def calculateTimeDiff(timeOne, timeTwo):
+    postStripOne = datetime.strptime(timeOne, "%H:%M:%S")
+    postStripTwo = datetime.strptime(timeTwo, "%H:%M:%S")
+    if postStripOne.hour == 23 and postStripTwo.hour > 23:
+        correctionPartOne = datetime(2021, 11, 25, 1)
+        correctionPartTwo = datetime(2021, 11, 25, 13)
+        correct = correctionPartOne - correctionPartTwo
+        postStripOne.__add__(correct)
+        postStripTwo.__add__(correct)
+    timeDifference = postStripTwo - postStripOne
+    return int(timeDifference.total_seconds())
+
+
+#Used in finding timing diagrams
+if __name__ == '__main__':
+    resultsSet = {}
+    resultsLink = []
+    for i in range(1, len(sys.argv), 1):
+        linkLines = []
+        totalTx = 0
+        confirmedTx = 0
+        with open(sys.argv[i], "r") as f:
+            fileList = list(f)
+            for line in fileList:
+                # If tx is submitted in the line
+                if line.find("intkey set") != -1:
+                    totalTx += 1
+                if line.find("show") != -1:
+                    confirmedTx += 1
+        resultsLink.append((confirmedTx, totalTx))
+    results = []
+    totalTotalTx = 0
+    totalConfirmedTx = 0
+    #For each in resultsLink go through link lines with correct time
+    #For each log file
+    resultsDict = {}
+    for e in resultsLink:
+        totalConfirmedTx += e[0]
+        totalTotalTx += e[1]
+    element = 0
+    with open("thoughputLogOutput.csv", "w") as l:
+        for tupe in resultsLink:
+            l.write("Experiment #: " + str(element) + "\n")
+            l.write(str(tupe[0])+", "+str(tupe[1])+"\n")
+        l.write("Total Confirmed: " + str(totalConfirmedTx) + "\n")
+        l.write("Total Submitted: " + str(totalTotalTx) + "\n")
+        element += 1
