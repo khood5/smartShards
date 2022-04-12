1f587297acf14fbec42ef37e638590c15d148608
WolfWindOW <nlwyborny@gmail.com>
2022-04-12 20:31:31 +0000

    Changed Logging file

--- evaluation/throughputLogs.py
+++ evaluation/throughputLogs.py
@@ -1,63 +1,55 @@
-import re
-from timingDiagramChurn import CHURN_RATES, EXPERIMENT_RANGE_END
+import sys
+from datetime import datetime
 
-experiments = EXPERIMENT_RANGE_END
-churnRates = CHURN_RATES
 
-logFormat = "logs/timingDiagramChurn.py.E{experiment}CR{churnRate}.SawtoothContainer.log"
+#Takes two times, of format HH:MM:SS and finds the difference
+def calculateTimeDiff(timeOne, timeTwo):
+    postStripOne = datetime.strptime(timeOne, "%H:%M:%S")
+    postStripTwo = datetime.strptime(timeTwo, "%H:%M:%S")
+    if postStripOne.hour == 23 and postStripTwo.hour > 23:
+        correctionPartOne = datetime(2021, 11, 25, 1)
+        correctionPartTwo = datetime(2021, 11, 25, 13)
+        correct = correctionPartOne - correctionPartTwo
+        postStripOne.__add__(correct)
+        postStripTwo.__add__(correct)
+    timeDifference = postStripTwo - postStripOne
+    return int(timeDifference.total_seconds())
 
-submitted_format = "(?P<tx_time>\d+:\d+:\d+) INFO \d+.\d+.\d+.\d+:running command: *intkey set (?P<tx_id>tx_\d+_\d+) (?P<tx_val>\d+)"
-confirmed_format = "(?P<tx_time>\d+:\d+:\d+) INFO \d+.\d+.\d+.\d+:command result: *(?P<tx_id>tx_\d+_\d+): (?P<tx_val>\d+)"
 
 #Used in finding timing diagrams
 if __name__ == '__main__':
-    for churnRate in churnRates:
-        resultsSet = {}
-        resultsLink = []
-        for experiment in range(experiments):
-            linkLines = []
-            totalTx = 0
-            confirmedTx = 0
-            inputFilename = logFormat.format(experiment=experiment, churnRate=churnRate)
-            print(f"opening {inputFilename}")
-            with open(inputFilename, "r") as inputFile:
-                logLines = list(inputFile)
-                for line in logLines:
-
-                    submit = re.search(submitted_format, line)
-                    confirm = re.search(confirmed_format, line)
-                    # If tx is submitted in the line
-                    if submit is not None:
-                        tx_id = submit.group('tx_id')
-                        tx_val = submit.group('tx_val')
-                        tx_time = submit.group('tx_time')
-                        print("submit")
-                        print(tx_id, tx_val, tx_time)
-                        if tx_val == "999":
-                            totalTx += 1
-                        
-                    if confirm is not None:
-                        tx_id = confirm.group('tx_id')
-                        tx_val = confirm.group('tx_val')
-                        tx_time = confirm.group('tx_time')
-                        print("confirm")
-                        print(tx_id, tx_val, tx_time)
-                        if tx_val == "999":
-                            confirmedTx += 1
-            resultsLink.append((confirmedTx, totalTx))
-        results = []
-        totalTotalTx = 0
-        totalConfirmedTx = 0
-        #For each in resultsLink go through link lines with correct time
-        #For each log file
-        resultsDict = {}
-        for e in resultsLink:
-            totalConfirmedTx += e[0]
-            totalTotalTx += e[1]
-        outputFilename = f"throughputLogOutputCR{churnRate}.csv"
-        print(f"writing {outputFilename}")
-        with open(outputFilename, "w") as outputFile:
-            outputFile.write(f"experiment,submitted,confirmed\n")
-            for experiment, (confirmed, submitted) in enumerate(resultsLink):
-                outputFile.write(f"{experiment},{submitted},{confirmed}\n")
-            outputFile.write(f"total,{totalTotalTx},{totalConfirmedTx}\n")
+    resultsSet = {}
+    resultsLink = []
+    for i in range(1, len(sys.argv), 1):
+        linkLines = []
+        totalTx = 0
+        confirmedTx = 0
+        prev = None
+        with open(sys.argv[i], "r") as f:
+            fileList = list(f)
+            for line in fileList:
+                # If tx is submitted in the line
+                if line.find("intkey set") != -1:
+                    totalTx += 1
+                if prev is not None:
+                    if line.find(": 999") != -1:
+                        confirmedTx += 1
+                prev = line
+        resultsLink.append((confirmedTx, totalTx))
+    results = []
+    totalTotalTx = 0
+    totalConfirmedTx = 0
+    #For each in resultsLink go through link lines with correct time
+    #For each log file
+    resultsDict = {}
+    for e in resultsLink:
+        totalConfirmedTx += e[0]
+        totalTotalTx += e[1]
+    element = 0
+    with open("throughputLogOutput.csv", "w") as l:
+        for tupe in resultsLink:
+            l.write("Experiment #: " + str(element) + "\n")
+            l.write(str(tupe[0])+", "+str(tupe[1])+"\n")
+        l.write("Total Confirmed: " + str(totalConfirmedTx) + "\n")
+        l.write("Total Submitted: " + str(totalTotalTx) + "\n")
+        element += 1
